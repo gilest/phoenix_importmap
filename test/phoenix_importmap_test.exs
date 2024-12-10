d881a7d03b7f3d9c7021b48b1bd0331e65ae587c
Giles Thompson <giles@papaiti.com>
2024-12-10 13:05:46 +1300

    Remove phoenix_html asset from test examples

--- test/phoenix_importmap_test.exs
+++ test/phoenix_importmap_test.exs
@@ -4,7 +4,6 @@ defmodule PhoenixImportmapTest do
 
   @example_importmap %{
     app: "/assets/js/app.js",
-    phoenix_html: "/deps/phoenix_html/priv/static/phoenix_html.js"
   }
 
   @example_missing_file_importmap %{
@@ -12,21 +11,14 @@ defmodule PhoenixImportmapTest do
   }
 
   test "copy succeeds" do
-    %{app: app_js_path, phoenix_html: dep_js_path} = @example_importmap
-
+    %{app: app_js_path} = @example_importmap
     app_js_full_path = File.cwd!() <> PhoenixImportmap.dest_path_for_asset(app_js_path)
-    dep_js_full_path = File.cwd!() <> PhoenixImportmap.dest_path_for_asset(dep_js_path)
 
     assert File.exists?(app_js_full_path) == false
-    assert File.exists?(dep_js_full_path) == false
-
     assert PhoenixImportmap.copy(@example_importmap) == :ok
-
     assert File.exists?(app_js_full_path)
-    assert File.exists?(dep_js_full_path)
 
     File.rm!(app_js_full_path)
-    File.rm!(dep_js_full_path)
   end
 
   test "copy fails on missing file" do
@@ -37,6 +29,6 @@ defmodule PhoenixImportmapTest do
 
   test "importmap_json" do
     assert PhoenixImportmap.importmap_json(@example_importmap) ==
-             "{\"imports\":{\"app\":\"/assets/app.js\",\"phoenix_html\":\"/assets/phoenix_html.js\"}}"
+             "{\"imports\":{\"app\":\"/assets/app.js\"}}"
   end
 end
