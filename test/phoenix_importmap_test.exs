7f0294d47cf1ce780e985fe82b6a8da586dced38
Giles Thompson <giles@papaiti.com>
2024-12-11 12:11:28 +1300

    Adjust directory setup for file testing

--- test/phoenix_importmap_test.exs
+++ test/phoenix_importmap_test.exs
@@ -10,15 +10,22 @@ defmodule PhoenixImportmapTest do
     app: "/assets/js/missing.js"
   }
 
+  setup do
+    File.mkdir_p!(File.cwd!() <> "/priv/static/assets")
+
+    on_exit(fn ->
+      File.rm_rf!(File.cwd!() <> "/priv/static/assets")
+      File.mkdir_p!(File.cwd!() <> "/priv/static/assets")
+    end)
+  end
+
   test "copy succeeds" do
     %{app: app_js_path} = @example_importmap
     app_js_full_path = File.cwd!() <> PhoenixImportmap.Asset.dest_path(app_js_path)
 
-    assert File.exists?(app_js_full_path) == false
+    assert !File.exists?(app_js_full_path)
     assert PhoenixImportmap.copy(@example_importmap) == :ok
     assert File.exists?(app_js_full_path)
-
-    File.rm!(app_js_full_path)
   end
 
   test "copy fails on missing file" do
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,1 @@
-File.rm_rf!(File.cwd!() <> "/priv/static/assets")
-File.mkdir_p!(File.cwd!() <> "/priv/static/assets")
 ExUnit.start()
