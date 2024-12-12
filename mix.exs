8bb2b2819656f5259c8138cce8c4c6c12b51d244
Giles Thompson <giles@papaiti.com>
2024-12-12 13:55:23 +1300

    Check support down to 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhoenixImportmap.MixProject do
     [
       app: :phoenix_importmap,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
