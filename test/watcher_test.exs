8b375b3feeabca39f18e722e356a7669fa7aaa7c
Giles Thompson <giles@papaiti.com>
2024-12-12 13:58:52 +1300

    Use tmp dir in WIP file watcher test

--- test/watcher_test.exs
+++ test/watcher_test.exs
@@ -1,12 +1,40 @@
 defmodule WatcherTest do
   use ExUnit.Case, async: true
 
+  alias PhoenixImportmap.Util
+
+  @moduletag :tmp_dir
+
   @example_importmap %{
     app: "/test/fixtures/js/app.js",
     remote: "https://cdn.es6/package.js"
   }
 
-  test "file watch" do
-    {:ok, _pid} = PhoenixImportmap.watch(@example_importmap, ~w(/test/fixtures))
+  setup %{tmp_dir: tmp_dir} do
+    relative_tmp_dir = Util.relative_path(tmp_dir)
+
+    Application.put_env(
+      :phoenix_importmap,
+      :copy_destination_path,
+      relative_tmp_dir <> "/assets"
+    )
+
+    Application.put_env(:phoenix_importmap, :public_asset_path_prefix, relative_tmp_dir)
+    File.mkdir_p!(tmp_dir <> "/assets")
+
+    {:ok, pid} =
+      start_supervised(
+        {PhoenixImportmap.Watcher,
+         %{
+           importmap: @example_importmap,
+           watch_dirs: ~w(/test/fixtures)
+         }}
+      )
+
+    %{pid: pid}
+  end
+
+  test "start supervised", %{pid: pid} do
+    assert pid
   end
 end
